eb2290160011d2753caf89ce5178a0e22caf4598
lggradosm <gabrielgradosmachado@gmail.com>
2024-06-21 15:35:00 -0500

    message

--- src/components/GallerySection/gallery-section.js
+++ src/components/GallerySection/gallery-section.js
@@ -15,7 +15,7 @@ export default function GallerySection(){
     <h3 className="animate-appearComponent font-bold text-6xl tracking-wide py-20">GALLERY</h3>
     <ul className=" grid  grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4  md:gap-2">
       { 
-        images.map((item,index)=>(<GalleryItem item={item} index={index} />))
+        images.map((item,index)=>(<GalleryItem key={index} item={item} index={index} />))
       }
     </ul>
    </section>
